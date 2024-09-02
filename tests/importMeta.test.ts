d8b4a8b367d32031846078489c3e5fe25dfd5224
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-09-02 20:56:14 +0900

    Add: Add import.meta test

--- tests/importMeta.test.ts
+++ tests/importMeta.test.ts
@@ -5,6 +5,7 @@
 import { fromFileUrl } from "jsr:@std/path@^1.0.2/from-file-url";
 import { createCrossTest } from "../mod.ts";
 import { assert } from "jsr:@std/assert";
+import { readdir, readFile } from "jsr:@cross/fs@^0.1.11";
 
 const crossTest = await createCrossTest(import.meta.url, {
   runtimes: ["deno", "node", "bun"],
@@ -18,8 +19,14 @@ crossTest("import.meta.filename is replaced", () => {
   const currentFile = fromFileUrl(import.meta.url);
   assert(import.meta.filename === currentFile);
 });
+crossTest("import.meta.filename is usable", async () => {
+  await readFile(import.meta.filename!);
+});
 
 crossTest("import.meta.dirname is replaced", () => {
   const currentFile = fromFileUrl(import.meta.url);
   assert(currentFile.startsWith(import.meta.dirname!));
+});
+crossTest("import.meta.dirname is usable", async () => {
+  await readdir(import.meta.dirname!);
 });
